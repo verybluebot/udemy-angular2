b893876d6c30a44e8f883c7cda7079252073d7d6
shu <verybluebot@gmail.com>
2017-02-19 23:18:41 +0200

    :ghost: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'hb-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hb works!';
+  title = 'hamsters book works!';
 }
